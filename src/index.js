f5818a3b09c64baf06503e40e5376080f76fcd41
cramosgamboa <c.ramosgamboa@gmail.com>
2021-09-14 22:55:29 -0300

    ultimos cambios 14.09

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import validator from './validator.js';
 
+const botonValidar = document.getElementById("Validartj");
+
+botonValidar.addEventListener("click", () => {
+  const numTarjeta = document.getElementById("numTarjeta");
+  const isValid = validator.isValid(numTarjeta.value);
+  const maskify = validator.maskify(numTarjeta.value);
+  console.log(maskify);
+
+
+// ALERTAS
+  if (isValid === true) {
+    alert("La tarjeta de crédito es válida");
+  } else {
+    alert("La tarjeta de crédito no es válida");
+  }
+
+
+
+
+
+});
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,28 @@
+//VALIDACION NUMEROS DE TARJETA DE CRÉDITO
 const validator = {
-  // ...
+  isValid: (numTarjeta) => {
+    let convertir = numTarjeta.split('').reverse();
+    let suma = 0;
+    let num = 0;
+    for (let index = 0; index < convertir.length; index++) {
+      if (index % 2 === 1) {
+        num = Number(convertir[index]) * 2;
+        if (num >= 10) {
+          num = (num- 10) + 1;
+        }
+      } else {
+        num = Number(convertir[index]);
+      }
+      suma += num;
+    }
+    return suma % 10 === 0 ? true : false;
+  },
+// ENMASCARAR NÚMEROS DE LA TARJETA 
+  maskify:(numTarjeta) =>{
+    return numTarjeta.split("").map((numero,idx)=> idx < numTarjeta.length -4 ? "#" : numero).join("");
+  }
 };
+
+
 
 export default validator;
